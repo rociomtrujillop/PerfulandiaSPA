cb22c8550e16d95285337650dec345b069a2f37b
gab1214 <ga.olguins@duocuc.cl>
2025-06-23 20:10:24 -0400

    Update ProductosRestController.java

--- src/main/java/com/proyectperfulandia/proyectoperfulandia/restcontroller/ProductosRestController.java
+++ src/main/java/com/proyectperfulandia/proyectoperfulandia/restcontroller/ProductosRestController.java
@@ -18,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.proyectperfulandia.proyectoperfulandia.entidades.Productos;
 import com.proyectperfulandia.proyectoperfulandia.servicios.ProductosServices;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping("api/productos")
 public class ProductosRestController {
