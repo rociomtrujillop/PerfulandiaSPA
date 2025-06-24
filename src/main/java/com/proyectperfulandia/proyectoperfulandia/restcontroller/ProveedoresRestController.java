1779e8d79fa27815ca80000da1cf6e92899e7fd4
gab1214 <ga.olguins@duocuc.cl>
2025-06-23 20:10:13 -0400

    Update ProveedoresRestController.java

--- src/main/java/com/proyectperfulandia/proyectoperfulandia/restcontroller/ProveedoresRestController.java
+++ src/main/java/com/proyectperfulandia/proyectoperfulandia/restcontroller/ProveedoresRestController.java
@@ -18,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.proyectperfulandia.proyectoperfulandia.entidades.Proveedores;
 import com.proyectperfulandia.proyectoperfulandia.servicios.ProveedoresServices;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping("api/proveedores")
 public class ProveedoresRestController {
